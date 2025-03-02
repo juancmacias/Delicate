c228b52c2482b785e4363dcae29421e215852fd6
jruizndev <jorsqn@gmail.com>
2025-03-02 18:19:47 +0100

    feat: Register Invoice model in admin panel with display and filter options

--- delicate_apps/invoices/admin.py
+++ delicate_apps/invoices/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Invoice
 
-# Register your models here.
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'payment_form', 'neto', 'fk_type', 'fk_user', 'fk_company')
+    list_filter = ('date', 'payment_form', 'fk_type', 'fk_company')
+    search_fields = ('id', 'payment_form')
+    date_hierarchy = 'date'
