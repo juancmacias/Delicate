c29ed1e790764db92732e6b380e40f3b5d01c8e4
jruizndev <jorsqn@gmail.com>
2025-03-02 18:16:01 +0100

    fix: Configure AppConfig correctly for Invoices application

--- delicate_apps/invoices/models.py
+++ delicate_apps/invoices/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from delicate_apps.users.models import User
+from delicate_apps.company.models import Company
+from delicate_apps.type.models import Type
 
-# Create your models here.
+class Invoice(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateField(verbose_name='Fecha')
+    payment_form = models.CharField(max_length=100, verbose_name='Forma de pago')
+    neto = models.FloatField(verbose_name='Importe neto')
+
+    fk_type =models.ForeignKey(
+        Type,
+        on_delete=models.CASCADE,
+        db_column='fk_type',
+        related_name='invoices',
+        verbose_name='Tipo'
+    )
+
+    fk_user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        db_column='fk_user',
+        related_name='invoices',
+        verbose_name='Usuario'
+    )
+
+    fk_company = models.ForeignKey(
+        Company,
+        on_delete=models.CASCADE,
+        db_column='fk_company',
+        related_name='invoices',
+        verbose_name='Empresa'
+    )
+
+    def __str__(self):
+        return f'Factura {self.id}'
+
+    class Meta:
+        verbose_name = 'Factura'
+        verbose_name_plural = 'Facturas'
+        db_table = 'invoices'
+
+    def get_total(self):
+        return self.neto
