c29b7475bbe43c339b7fee087044ac4842d74449
jruizndev <jorsqn@gmail.com>
2025-03-02 13:15:51 +0100

    fix: Resolve conflict with environment variables in database (settings.py) configuration

--- delicate_manager/settings.py
+++ delicate_manager/settings.py
@@ -91,11 +91,11 @@ WSGI_APPLICATION = 'delicate_manager.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("NAME"),
-        'USER': os.environ.get("USER"),
-        'PASSWORD': os.environ.get("PASSWORD"),
-        'HOST': os.environ.get("HOST"),
-        'PORT': os.environ.get("PORT"),
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
--- delicate_manager/urls.py
+++ delicate_manager/urls.py
@@ -19,15 +19,14 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
     # Agrupar rutas de la API bajo una versión específica
-    path('v1/api/', include([
-        path('company/', include('delicate_apps.company.urls')),
-        path('users/', include('delicate_apps.users.urls')),
-        path('store/', include('delicate_apps.store.urls')),
-        path('invoices/', include('delicate_apps.invoices.urls')),
-        path('basket/', include('delicate_apps.basket.urls')),
-        path('type/', include('delicate_apps.type.urls')),
-    ])),
+    # path('v1/api/', include([
+    #     path('company/', include('delicate_apps.company.urls')),
+    #     path('users/', include('delicate_apps.users.urls')),
+    #     path('store/', include('delicate_apps.store.urls')),
+    #     path('invoices/', include('delicate_apps.invoices.urls')),
+    #     path('basket/', include('delicate_apps.basket.urls')),
+    #     path('type/', include('delicate_apps.type.urls')),
+    # ])),
 ]
 
