70af0909897c00354deaed84e7e204ccea7b8154
jruizndev <jorsqn@gmail.com>
2025-03-02 13:32:47 +0100

    feat: Implement Type model for business categories

--- delicate_apps/type/models.py
+++ delicate_apps/type/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Type(models.Model):
+    id = models.AutoField(primary_key=True)
+    name_type = models.CharField(max_length=30, verbose_name="Tipo de comercio")
+
+    def __str__(self):
+        return self.name_type
+
+        class Meta:
+            verbose_name = "Tipo de comercio"
+            verbose_name_plural = "Tipos de comercio"
+            db_table = 'type'
