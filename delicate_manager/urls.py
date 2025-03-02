eba2f9df60b113324947eb730b92658f20b485de
jruizndev <jorsqn@gmail.com>
2025-03-02 18:32:59 +0100

    feat: Add Invoice API routes to main URL configuration

--- delicate_manager/urls.py
+++ delicate_manager/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     #     path('company/', include('delicate_apps.company.urls')),
     #     path('users/', include('delicate_apps.users.urls')),
     path('store/', include('delicate_apps.store.urls')),
-    #     path('invoices/', include('delicate_apps.invoices.urls')),
+    path('invoices/', include('delicate_apps.invoices.urls')),
     #     path('basket/', include('delicate_apps.basket.urls')),
     path('type/', include('delicate_apps.type.urls')),
     ])),
