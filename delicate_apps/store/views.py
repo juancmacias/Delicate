862dd8ce21c74f91e117f93b6e3704efe043168c
jruizndev <jorsqn@gmail.com>
2025-03-02 15:47:43 +0100

    fix: Correct typo in import statement for get_object_or_404

--- delicate_apps/store/views.py
+++ delicate_apps/store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_objectt_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
