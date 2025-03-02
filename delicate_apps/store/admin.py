8329fbc1624462b6afb87481f63fd4ea784136e5
jruizndev <jorsqn@gmail.com>
2025-03-02 15:26:47 +0100

    feat: Register StoreProduct in admin panel with display and filter options

--- delicate_apps/store/admin.py
+++ delicate_apps/store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import StoreProduct
 
-# Register your models here.
+@admin.register(StoreProduct)
+class StoreProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'net_price', 'iva', 'fk_company', 'fk_type')
+    list_filter = ('category', 'fk_type', 'fk_company')
+    search_fields = ('name', 'description', 'category')
