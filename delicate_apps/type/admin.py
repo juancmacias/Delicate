ee2c0ae77c5896f0f644db087abbebdae2708c27
jruizndev <jorsqn@gmail.com>
2025-03-02 13:35:22 +0100

    feat: Register Type model in admin panel with search functionality

--- delicate_apps/type/admin.py
+++ delicate_apps/type/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Type
 
-# Register your models here.
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name_type')
+    search_fields = ('name_type',)
