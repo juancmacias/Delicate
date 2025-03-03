0d7c072d993997c20614232e308227f62800db93
jruizndev <jorsqn@gmail.com>
2025-03-03 09:38:13 +0100

    feat: Register BasketTemp model in admin panel with display and filter options

--- delicate_apps/basket/admin.py
+++ delicate_apps/basket/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BasketTemp
+
+@admin.register(BasketTemp)
+class BasketTempAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_id', 'product_id', 'cantidad', 'precio', 'temp_date')
+    list_filter = ('user_id', 'temp_date')
+    search_fields = ('user_id__username', 'product_id__name')
+    date_hierarchy = 'temp_date'
