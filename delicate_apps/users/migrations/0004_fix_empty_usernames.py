71286ca5f206acf4c729df64d385582f72a4a093
jruizndev <jorsqn@gmail.com>
2025-03-05 09:38:36 +0100

    Fix empty usernames migration

---
+++ delicate_apps/users/migrations/0004_fix_empty_usernames.py
@@ -0,0 +1,28 @@
+from django.db import migrations
+
+def fix_empty_usernames(apps, schema_editor):
+    User = apps.get_model('users', 'User')
+    users_with_empty_username = User.objects.filter(username='')
+    
+    for user in users_with_empty_username:
+        base_username = user.email.split('@')[0]
+        username = base_username
+        count = 1
+        
+        # Buscar un username único
+        while User.objects.filter(username=username).exists() and username != '':
+            username = f"{base_username}{count}"
+            count += 1
+        
+        user.username = username
+        user.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0003_fix_admin_log_user_references'),  # Ajusta esto a tu última migración
+    ]
+
+    operations = [
+        migrations.RunPython(fix_empty_usernames),
+    ]
