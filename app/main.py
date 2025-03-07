40dd6e533b1c8f9b93f2a0a36873406f0a0db38f
juan carlos macias <juancmaciassalvador@gmail.com>
2025-03-07 13:23:46 +0100

    Bug: change requirements.txt

--- app/main.py
+++ app/main.py
@@ -51,12 +51,6 @@ api_v1.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory = "templates")
 
 
-
-#SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-#ALGORITHM = "HS256"
-#ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
 # Seguridad
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
